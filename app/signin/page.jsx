9b229003aacb17fa9314405b31a12c06446d68cf
Ashwin Sharma <sharmaashwin203@gmail.com>
2024-01-21 01:38:45 +0530

    first commit

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -1,14 +1,22 @@
 "use client";
 import SignInpage from "../../components/SignInpage";
 import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 import React from "react";
 const SignIn = () => {
   const { status, data: session } = useSession();
-  if (session?.user?.image) {
-    redirect("/");
+  const router = useRouter();
+  if (status === "loading" || status === "authenticated") {
+    router.push("/");
+    router.refresh();
   }
-  return <div>{!session?.user?.image && <SignInpage />}</div>;
+  return (
+    status === "unauthenticated" && (
+      <div>
+        <SignInpage />
+      </div>
+    )
+  );
 };
 
 export default SignIn;
