56ab04b1a5f78fe107f9456b75aba5f92f804e0f
Ashwin Sharma <sharmaashwin203@gmail.com>
2024-01-23 03:55:26 +0530

    first commit

--- app/orderstatus/page.jsx
+++ app/orderstatus/page.jsx
@@ -19,71 +19,40 @@ const SuccessPage = ({ searchParams }) => {
     }
   }, [session_id]);
 
-  return session_id ? (
-    <div>
-      <div className=" flex flex-col justify-center items-center w-full min-h-[74vh] ">
-        <motion.div
-          initial={{ opacity: 0, scale: 0.2 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.6 }}
-        >
-          <Image
-            src={successImg}
-            alt="Empty Cart "
-            priority={true}
-            className="w-[400px] h-auto"
-          />
-        </motion.div>
-        <div className=" mt-6">
-          <h1 className=" font-bold text-2xl text-center sm:text-3xl bg-gradient-to-r from-slate-700 via-purple-900 to-slate-700 inline-block text-transparent bg-clip-text mb-6">
-            Congratulations! Your order has been created successfully.
-          </h1>
-        </div>
-        <Link
-          href={"/"}
-          aria-label="Home Link"
-          className=" flex gap-2 items-center justify-center align-middle py-2  px-3 sm:px-4 text-gray-200 font-medium text-sm bg-gradient-to-r from-slate-700 via-purple-600 to-slate-700 
+  return (
+    session_id && (
+      <div>
+        <div className=" flex flex-col justify-center items-center w-full min-h-[74vh] ">
+          <motion.div
+            initial={{ opacity: 0, scale: 0.2 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.6 }}
+          >
+            <Image
+              src={successImg}
+              alt="Empty Cart "
+              priority={true}
+              className="w-[400px] h-auto"
+            />
+          </motion.div>
+          <div className=" mt-6">
+            <h1 className=" font-bold text-2xl text-center sm:text-3xl bg-gradient-to-r from-slate-700 via-purple-900 to-slate-700 inline-block text-transparent bg-clip-text mb-6">
+              Congratulations! Your order has been created successfully.
+            </h1>
+          </div>
+          <Link
+            href={"/"}
+            aria-label="Home Link"
+            className=" flex gap-2 items-center justify-center align-middle py-2  px-3 sm:px-4 text-gray-200 font-medium text-sm bg-gradient-to-r from-slate-700 via-purple-600 to-slate-700 
                 rounded-md cursor-pointer 
              hover:scale-[.98] transition-all duration-300 "
-        >
-          <BiShoppingBag size={22} />
-          Continue Shopping
-        </Link>
-      </div>
-    </div>
-  ) : (
-    <>
-      <div className=" flex flex-col justify-center items-center w-full min-h-[74vh]">
-        <motion.div
-          initial={{ opacity: 0, scale: 0.2 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.6 }}
-        >
-          <Image
-            src={order}
-            alt="Empty Cart "
-            priority={true}
-            className="w-[400px] h-auto"
-          />
-        </motion.div>
-
-        <div className="mt-6">
-          <h1 className=" font-bold text-2xl sm:text-3xl text-center bg-gradient-to-r from-slate-700 via-purple-900 to-slate-700 inline-block text-transparent bg-clip-text mb-6">
-            Please order something before accessing this page.
-          </h1>
+          >
+            <BiShoppingBag size={22} />
+            Continue Shopping
+          </Link>
         </div>
-        <Link
-          href={"/products"}
-          aria-label="Home Link"
-          className=" flex gap-2 items-center justify-center align-middle py-2  px-3 sm:px-4 text-gray-200 font-medium text-sm bg-gradient-to-r from-slate-700 via-purple-600 to-slate-700 
-                rounded-md cursor-pointer 
-             hover:scale-[.98] transition-all duration-300 "
-        >
-          <BiShoppingBag size={22} />
-          Continue Shopping
-        </Link>
       </div>
-    </>
+    )
   );
 };
 
